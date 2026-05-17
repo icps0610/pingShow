f03adebc519a4e51557da02d078adb2ffd0d5089
icps0610 <icps0610@gmail.com>
2026-05-17 16:13:24 +0800

    2026-05-17-16:13:24

--- cmd/pingShow/pingShow.go
+++ cmd/pingShow/pingShow.go
@@ -2,38 +2,54 @@ package main
 
 import (
 	"fmt"
+
 	"pingShow/internal/models"
 	"pingShow/internal/router"
 	"pingShow/internal/service"
+	"pingShow/pkg/cmdutils"
 )
 
 func main() {
-	// 載入或建立本地監控目標設定檔 (target.json)
-	if err := models.LoadTargets(); err != nil {
-		fmt.Printf("⚠️ 無法載入 target.json: %v，採用內建監控目標\n", err)
+	appVersion := `1.0`
+	argsCount := 0
+	description := `PingShow 網路連線狀態即時監測系統 - 提供即時與歷史的 Ping 延遲圖表監控，極致輕量且支援背景執行。
+支援於 target.json 中自訂網頁連接埠(port)與監控目標(IP)。`
+	useage := `Usage: pingShow`
+	{
+		cmdutils.Set(useage, description)
+		cmdutils.SetArgs(argsCount, appVersion)
 	}
 
-	// 啟動背景服務
-	go service.StartLogWriter()
+	// "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="z:\Temp\Chrome DevSession"
+	{
+		// 載入或建立本地監控目標設定檔 (target.json)
+		if err := models.LoadTargets(); err != nil {
+			fmt.Printf("⚠️ 無法載入 target.json: %v，採用內建監控目標\n", err)
+		}
 
-	// 使用 mutex 或複製的 slice 啟動 worker 確保安全
-	models.DataMutex.Lock()
-	targetsToStart := make([]*models.TargetInfo, len(models.Targets))
-	copy(targetsToStart, models.Targets)
-	models.DataMutex.Unlock()
+		// 啟動背景服務
+		go service.StartLogWriter()
 
-	for _, t := range targetsToStart {
-		go service.StartPingWorker(t)
-	}
-	go service.StartSnapshotTicker()
+		// 使用 mutex 或複製的 slice 啟動 worker 確保安全
+		models.DataMutex.Lock()
+		targetsToStart := make([]*models.TargetInfo, len(models.Targets))
+		copy(targetsToStart, models.Targets)
+		models.DataMutex.Unlock()
 
-	// 設定與啟動 Gin Router
-	r := router.SetupRouter()
+		for _, t := range targetsToStart {
+			go service.StartPingWorker(t)
+		}
+		go service.StartSnapshotTicker()
 
-	fmt.Println("網路連線狀態即時監測系統已啟動！歷史數據同步寫入 ./logs 夾")
-	fmt.Printf("請開啟瀏覽器: http://localhost:%d\n", models.SystemPort)
+		// 設定與啟動 Gin Router
+		r := router.SetupRouter()
 
-	if err := r.Run(fmt.Sprintf(":%d", models.SystemPort)); err != nil {
-		fmt.Printf("伺服器啟動失敗: %v\n", err)
+		fmt.Println("網路連線狀態即時監測系統已啟動！歷史數據同步寫入 ./logs 夾")
+		fmt.Printf("請開啟瀏覽器: http://localhost:%d\n", models.SystemPort)
+
+		if err := r.Run(fmt.Sprintf(":%d", models.SystemPort)); err != nil {
+			fmt.Printf("伺服器啟動失敗: %v\n", err)
+		}
+
 	}
 }
